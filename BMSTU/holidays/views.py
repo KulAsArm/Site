8be1c4b47711b6b50477c9cb748fba3c4947de7f
Илья Шафф <pro.shaff@mail.ru>
2023-07-17 21:41:44 +0300

    Updated Destinations page, added 8 booking pages, progressbar logic, model of destinations in DB

--- BMSTU/holidays/views.py
+++ BMSTU/holidays/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Destinations
 
 
 def index(request):
@@ -9,10 +10,80 @@ def index(request):
     return HttpResponse(template.render(context, request))
 
 
+
+def booking2(request):
+    template = loader.get_template("holidays/booking2.html")
+    dest=Destinations.objects.get(id=1)
+    occ=dest.occ_seats
+    tot=dest.tot_seats
+    res=int((occ/tot)*100)
+    context = {'seat_num':res }
+    print(res)
+    return HttpResponse(template.render(context, request))
+def booking3(request):
+    template = loader.get_template("holidays/booking3.html")
+    dest=Destinations.objects.get(id=1)
+    occ=dest.occ_seats
+    tot=dest.tot_seats
+    res=int((occ/tot)*100)
+    context = {'seat_num':res }
+    print(res)
+    return HttpResponse(template.render(context, request))
+def booking4(request):
+    template = loader.get_template("holidays/booking4.html")
+    dest=Destinations.objects.get(id=1)
+    occ=dest.occ_seats
+    tot=dest.tot_seats
+    res=int((occ/tot)*100)
+    context = {'seat_num':res }
+    print(res)
+    return HttpResponse(template.render(context, request))
+def booking5(request):
+    template = loader.get_template("holidays/booking5.html")
+    dest=Destinations.objects.get(id=1)
+    occ=dest.occ_seats
+    tot=dest.tot_seats
+    res=int((occ/tot)*100)
+    context = {'seat_num':res }
+    print(res)
+    return HttpResponse(template.render(context, request))
+def booking6(request):
+    template = loader.get_template("holidays/booking6.html")
+    dest=Destinations.objects.get(id=1)
+    occ=dest.occ_seats
+    tot=dest.tot_seats
+    res=int((occ/tot)*100)
+    context = {'seat_num':res }
+    print(res)
+    return HttpResponse(template.render(context, request))
+def booking7(request):
+    template = loader.get_template("holidays/booking7.html")
+    dest=Destinations.objects.get(id=1)
+    occ=dest.occ_seats
+    tot=dest.tot_seats
+    res=int((occ/tot)*100)
+    context = {'seat_num':res }
+    print(res)
+    return HttpResponse(template.render(context, request))
+def booking8(request):
+    template = loader.get_template("holidays/booking8.html")
+    dest=Destinations.objects.get(id=1)
+    occ=dest.occ_seats
+    tot=dest.tot_seats
+    res=int((occ/tot)*100)
+    context = {'seat_num':res }
+    print(res)
+    return HttpResponse(template.render(context, request))
 def booking(request):
     template = loader.get_template("holidays/booking.html")
-    context = {}
+    dest=Destinations.objects.get(id=1)
+    occ=dest.occ_seats
+    tot=dest.tot_seats
+    res=int((occ/tot)*100)
+    context = {'seat_num':res }
+    print(res)
     return HttpResponse(template.render(context, request))
+
 
 
 def aboutus(request):
